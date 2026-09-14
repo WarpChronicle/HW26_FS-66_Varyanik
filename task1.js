4689d1576fd6ad1f6ee63ebaae3d1c791f292e7f
WarpChronicle <Varyanik98@gmail.com>
2026-09-14 17:53:24 +0300

    final_commit

---
+++ task1.js
@@ -0,0 +1,57 @@
+/*
+Используя два информационных ресурса (API) - https://jsonplaceholder.typicode.com/users и https://api.open-meteo.com/v1/forecast?latitude=44.49&longitude=20.27&current_weather=true
+1. Получить список пользователей (users) с ресурса https://jsonplaceholder.typicode.com/users
+2. Для каждого пользователя получить его географические координаты (latitude и longitude)  
+3. Используя эти координаты, получить текущую погоду для каждого пользователя с ресурса https://api.open-meteo.com/v1/forecast?latitude=44.49&longitude=20.27&current_weather=true
+4. Определить пользователя с самой высокой температурой и вывести его имя, телефон  
+ и температуру в консоль.
+ 
+ Решите задачу с использованием
+ 5.fetch  
+ 6.axios (для одного из запросов).
+ 
+*/
+
+async function returnUser(id) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  return await response.json();
+}
+
+async function returnWeather(latitude, longitude) {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+  const response = await fetch(url);
+  return await response.json();
+}
+
+const persons = [];
+
+for (let i = 1; i <= 10; i++) {
+  
+  const userData = await returnUser(i);
+  
+  const lat = userData.address.geo.lat;
+  const lng = userData.address.geo.lng;
+
+  const weatherData = await returnWeather(lat, lng);
+
+  let myObj = {
+    id: userData.id,
+    name: userData.name,
+    phone: userData.phone,
+    latitude: lat,
+    longitude: lng,
+    temperature: weatherData.current_weather.temperature,
+    windspeed: weatherData.current_weather.windspeed,
+    winddirection: weatherData.current_weather.winddirection,
+  };
+
+  persons.push(myObj);
+}
+
+console.log(persons);
+
+const hottestTemperature = persons.reduce((max, user) => {
+  return user.temperature > max.temperature ? user : max;
+}, persons[0]);
+
+console.log('Пользователь с самой высокой температурой:', hottestTemperature);
